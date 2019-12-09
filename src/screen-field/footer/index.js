66c1f0a4214ae02b6ec597080519dd032c40d22e
Evandro Lutz <evandrolutz@outlook.com>
2019-12-09 00:11:49 -0300

    featured footer prototype

--- src/screen-field/footer/index.js
+++ src/screen-field/footer/index.js
@@ -1,3 +1,32 @@
-import React from  'react';
+import React from 'react';
 import * as Styled from './style';
+import ifrs from '../../img/ifrs.svg';
+import youtube from '../../img/youtube.svg';
+import fb from '../../img/fb.svg';
 
+const footer = () => (
+    <Styled.footer>
+        <Styled.footerContent>
+            <Styled.Titulo>Contatos:</Styled.Titulo>
+            <Styled.Titulo>Endereço:</Styled.Titulo>
+            <Styled.Titulo>Coordenação:</Styled.Titulo>
+            <Styled.Titulo>Extensão:
+                <Styled.LogoBox>
+                    <img alt='Logo do ifrs' src={ifrs} /><Styled.conteudo>Instituto Federal Rio Grande do Sul</Styled.conteudo>
+                </Styled.LogoBox>
+            </Styled.Titulo>
+            <Styled.Titulo>Follow:
+                <Styled.LogoBox>
+                    <img alt='Logo do youtube' src={youtube} />
+                    <img alt='Logo do fb' src={fb} />
+                </Styled.LogoBox>
+            </Styled.Titulo>
+        </Styled.footerContent>
+    </Styled.footer>
+);
+
+
+
+
+
+export default footer;
--- src/screen-field/footer/style.js
+++ src/screen-field/footer/style.js
@@ -1,1 +1,65 @@
 import Styled from 'styled-components';
+
+export const footer = Styled.footer`
+    padding: 15px 10px;
+    background: #458C8C;
+    height: 73px;
+
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    `;
+
+export const footerContent = Styled.div`
+    display: flex;
+    max-width: 800px;
+    margin: 0px auto;
+    justify-content: space-around;
+    height: 100%;
+`;
+
+
+export const LogoBox = Styled.div`
+    display: flex;
+    align-items: center;
+    width:80px;
+    height: 100%;
+    img {
+        max-width: 100%;
+    }
+`;
+
+export const Titulo = Styled.b`
+    position: relative;
+    width: 87px;
+    height: 40px;
+    
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 14px;
+
+    color: #FFFFFF;
+    `;
+
+export const conteudo= Styled.b`
+    position: relative;
+    width: 87px;
+    height: 40px;
+
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 8px;
+    line-height: 14px;
+
+    color: #FFFFFF;
+`;
+
+
+
+
