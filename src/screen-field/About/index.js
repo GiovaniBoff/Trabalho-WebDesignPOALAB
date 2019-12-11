5ab1c37574c300c5afc70028ca9deed11ed119db
Giovani Boff <giovanisouza15@gmail.com>
2019-12-11 01:39:53 -0300

    correcting about component

--- src/screen-field/About/index.js
+++ src/screen-field/About/index.js
@@ -1,11 +1,33 @@
 import * as React from 'react';
-import AboutComponent from './AboutComponent'
+import ImagePoaLab from '../../img/poalab.jpg'
 import Cards from '../../components/Cards';
 import BackgroundImage from '../../img/laserWooden.jpg';
 import Icon from '../../img/aboutIcon.svg';
+import * as Styled from './styled';
 const About = () => (
     <Cards title="Sobre" imgBackground={BackgroundImage} icon={Icon}>
-        <AboutComponent/>
+         <Styled.Container>
+            <img src={ImagePoaLab}/>
+            <p>
+                O POALAB é um laboratório de fabricação digital
+                e está conectado à rede mundial de fab labs.
+                Opera como um programa de extensão do
+                IFRS campus Porto Alegre.
+        
+                Temos como missão popularizar o acesso
+                e letramento na fabricação digital,
+                funcionando como uma plataforma de criatividade,
+                aprendizagem e invenção.
+
+                Ser um Fab Lab significa estar conectado
+                a uma comunidade mundial de alunos, educadores,
+                técnicos, pesquisadores e inovadores e desenvolver
+                atividades dentro da filosofia do conhecimento aberto.
+
+                Conheça a FAB CHARTER
+                A Carta de Intenções que regra os Fab Labs  
+            </p>
+        </Styled.Container>
     </Cards>
 );
 
