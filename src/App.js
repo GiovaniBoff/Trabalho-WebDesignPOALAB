7fc4cefc3d348bd4d40f1a27b472dba1f546e3a4
Nikolas Freitas <nikolasbvf@gmail.com>
2019-12-12 03:40:20 -0300

    :sparkles: Anchor the links

--- src/App.js
+++ src/App.js
@@ -14,12 +14,15 @@ function App() {
     <div>
       <Header/>
       <section className="delimiter">
+        <span id="About"/>
         <LazyLoad offset={-100} height="420" placeholder={<Spinner animation="border"/>}>
           <About />
         </LazyLoad>
+        <span id="Equipments"/>
         <LazyLoad offset={-100} height="420" placeholder={<Spinner animation="border"/>}>
           <Equipament />
         </LazyLoad>
+        <span id="OpenDay"/>
         <LazyLoad offset={-100} height="420" placeholder={<Spinner animation="border"/>}>
           <OpenDay />
         </LazyLoad>
