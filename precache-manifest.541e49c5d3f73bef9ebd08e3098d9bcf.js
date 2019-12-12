d755e1671d4f6462777bf2345db64ac15abd9e32
Nikolas Freitas <nikolasbvf@gmail.com>
2019-12-12 02:32:09 -0300

    Updates

---
+++ precache-manifest.541e49c5d3f73bef9ebd08e3098d9bcf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2d27b06d8a7d8014833a8e663b68c3",
+    "url": "/Trabalho-WebDesignPOALAB/index.html"
+  },
+  {
+    "revision": "97051baa38ac7233eaac",
+    "url": "/Trabalho-WebDesignPOALAB/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1243fed44e0527f02b60",
+    "url": "/Trabalho-WebDesignPOALAB/static/css/main.7ba292fd.chunk.css"
+  },
+  {
+    "revision": "97051baa38ac7233eaac",
+    "url": "/Trabalho-WebDesignPOALAB/static/js/2.7cfeec2e.chunk.js"
+  },
+  {
+    "revision": "7e5a07cb00bc8c8b922635a3cc993e4d",
+    "url": "/Trabalho-WebDesignPOALAB/static/js/2.7cfeec2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1243fed44e0527f02b60",
+    "url": "/Trabalho-WebDesignPOALAB/static/js/main.476d3441.chunk.js"
+  },
+  {
+    "revision": "8f558d43cfd603e1520a",
+    "url": "/Trabalho-WebDesignPOALAB/static/js/runtime-main.cded86b9.js"
+  },
+  {
+    "revision": "fd9d0a8555fa61e6c749aa5782a23162",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/3d.fd9d0a85.jpg"
+  },
+  {
+    "revision": "4fbe25df515485f377057d633b3d3f4d",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/DSC_4946.4fbe25df.jpg"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "c9d766fc3f28b478eb68bce61dd67fcd",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/aboutIcon.c9d766fc.svg"
+  },
+  {
+    "revision": "f265c72f2e2d186d42a2f7caa98e9fa7",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/engineIcon.f265c72f.svg"
+  },
+  {
+    "revision": "a7b482bd00925320d368696220dcb1da",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/fb.a7b482bd.svg"
+  },
+  {
+    "revision": "cad7d9bd11b0bcc09d144e2987db96bb",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/hand.cad7d9bd.jpg"
+  },
+  {
+    "revision": "ba317344518c431f2e184d2d13e2acf5",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/ifrs.ba317344.svg"
+  },
+  {
+    "revision": "0429b0639e0758b6737248176f78ea98",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/laserWooden.0429b063.jpg"
+  },
+  {
+    "revision": "f879eee0ab17d78dd0507b45176237e5",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/logo.f879eee0.svg"
+  },
+  {
+    "revision": "31ba89e9e33d19f9cbd91e3813ec3094",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/openDayIcon.31ba89e9.svg"
+  },
+  {
+    "revision": "304e03ee07fc34854981859c465b8d86",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/poalab.304e03ee.jpg"
+  },
+  {
+    "revision": "07f075496aeec13171e11dd40716b95d",
+    "url": "/Trabalho-WebDesignPOALAB/static/media/youtube.07f07549.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efe113b8df6ee72643be869f47194de3.js"
+  "/Trabalho-WebDesignPOALAB/precache-manifest.541e49c5d3f73bef9ebd08e3098d9bcf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Trabalho-WebDesignPOALAB/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
