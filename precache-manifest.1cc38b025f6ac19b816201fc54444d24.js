882861166840ff1a92f3b70006a852f8c8069b3b
Nikolas Freitas <nikolasbvf@gmail.com>
2019-12-12 01:09:21 -0300

    Updates

---
+++ precache-manifest.1cc38b025f6ac19b816201fc54444d24.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9649fd3bed90f574f2b4e257ea28ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca03f731845213681d7",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1c0058c3768cfcc09a43",
+    "url": "/static/css/main.35dabda0.chunk.css"
+  },
+  {
+    "revision": "6ca03f731845213681d7",
+    "url": "/static/js/2.0c579040.chunk.js"
+  },
+  {
+    "revision": "7e5a07cb00bc8c8b922635a3cc993e4d",
+    "url": "/static/js/2.0c579040.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c0058c3768cfcc09a43",
+    "url": "/static/js/main.2ed9caac.chunk.js"
+  },
+  {
+    "revision": "8989e7ac8f7a8a7ad072",
+    "url": "/static/js/runtime-main.303263ff.js"
+  },
+  {
+    "revision": "fd9d0a8555fa61e6c749aa5782a23162",
+    "url": "/static/media/3d.fd9d0a85.jpg"
+  },
+  {
+    "revision": "4fbe25df515485f377057d633b3d3f4d",
+    "url": "/static/media/DSC_4946.4fbe25df.jpg"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "c9d766fc3f28b478eb68bce61dd67fcd",
+    "url": "/static/media/aboutIcon.c9d766fc.svg"
+  },
+  {
+    "revision": "f265c72f2e2d186d42a2f7caa98e9fa7",
+    "url": "/static/media/engineIcon.f265c72f.svg"
+  },
+  {
+    "revision": "a7b482bd00925320d368696220dcb1da",
+    "url": "/static/media/fb.a7b482bd.svg"
+  },
+  {
+    "revision": "cad7d9bd11b0bcc09d144e2987db96bb",
+    "url": "/static/media/hand.cad7d9bd.jpg"
+  },
+  {
+    "revision": "ba317344518c431f2e184d2d13e2acf5",
+    "url": "/static/media/ifrs.ba317344.svg"
+  },
+  {
+    "revision": "0429b0639e0758b6737248176f78ea98",
+    "url": "/static/media/laserWooden.0429b063.jpg"
+  },
+  {
+    "revision": "f879eee0ab17d78dd0507b45176237e5",
+    "url": "/static/media/logo.f879eee0.svg"
+  },
+  {
+    "revision": "31ba89e9e33d19f9cbd91e3813ec3094",
+    "url": "/static/media/openDayIcon.31ba89e9.svg"
+  },
+  {
+    "revision": "304e03ee07fc34854981859c465b8d86",
+    "url": "/static/media/poalab.304e03ee.jpg"
+  },
+  {
+    "revision": "07f075496aeec13171e11dd40716b95d",
+    "url": "/static/media/youtube.07f07549.svg"
+  }
+]);
